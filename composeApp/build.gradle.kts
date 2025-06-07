3eee15fa0b1d8ae7d077a7b82ede46f494cc9456
Marc DiNino <dinino_marc@bah.com>
2025-06-07 18:27:14 -0400

    Fix gradle

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -69,10 +69,9 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation(libs.koin.core)
+            api(libs.koin.core)
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
-            implementation(libs.navigation.compose)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
