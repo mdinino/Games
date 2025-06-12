3948a84d5b01133b8778d5426041af8abed828b2
Marc DiNino <dinino_marc@bah.com>
2025-06-12 17:55:20 -0400

    Make buttons match

--- composeApp/src/commonMain/kotlin/dinino/marc/games/ui/screen/selectgames/SelectGameScreen.kt
+++ composeApp/src/commonMain/kotlin/dinino/marc/games/ui/screen/selectgames/SelectGameScreen.kt
@@ -5,11 +5,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import dinino.marc.games.ui.screen.GamesSnackbarController
 import dinino.marc.games.ui.screen.ObserveOneTimeEventEffects.ObserveOneTimeEventsOrNullEffect
 import games.composeapp.generated.resources.Res
@@ -57,29 +71,74 @@ fun SelectGameScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(stringResource(Res.string.select_game))
-        GameButtons(onTicTacToeSelected, onTetrisSelected)
+        GameButtonsColumn(
+            modifier = Modifier
+                .safeContentPadding()
+                .fillMaxSize()
+                .wrapContentWidth(),
+            onTicTacToeSelected =onTicTacToeSelected,
+            onTetrisSelected = onTetrisSelected)
     }
 }
 
-@Composable
 @Preview
-private fun GameButtons(
+@Composable
+fun GameButtonsColumn(
+    modifier: Modifier = Modifier,
     onTicTacToeSelected : ()->Unit = {},
     onTetrisSelected : ()->Unit = {}
 ) {
+    val density: Density = LocalDensity.current
+    val measuredWidths = remember{
+        mutableStateOf<Map<String, Dp?>>(emptyMap())
+    }
+
+    fun buttonModifier(id: String) =
+        when(measuredWidths[id]) {
+            null -> Modifier.wrapContentWidth()
+                .onPlaced {
+                    measuredWidths[id] = with(density) { it.size.width.toDp() }
+                }
+            else -> Modifier.width(measuredWidths.maxSafe())
+        }
+
     Column(
-        modifier = Modifier
-            .safeContentPadding()
-            .fillMaxSize(),
+        modifier = modifier,
         verticalArrangement = Arrangement.Center,
     ) {
         Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = onTicTacToeSelected
+            onClick = onTicTacToeSelected,
+            modifier = buttonModifier("TicTacToe"),
         ) { Text(stringResource(Res.string.game_tictactoe)) }
         Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = onTetrisSelected
+            onClick = onTetrisSelected,
+            modifier = buttonModifier("Tetris")
         ) { Text(stringResource(Res.string.game_tetris)) }
     }
 }
+
+private fun MutableState<Map<String, Dp?>>.maxSafe(): Dp =
+    max() ?: 0.dp
+private fun MutableState<Map<String, Dp?>>.max(): Dp? {
+    var max: Dp? = null
+    for (value in this.value.values) {
+        if (value == null) continue
+        else if (max == null) max = value
+        else if (value > max) max = value
+    }
+    return max
+}
+
+private operator fun MutableState<Map<String, Dp?>>.get(key: String): Dp? =
+    this.value[key]
+
+private operator fun MutableState<Map<String, Dp?>>.set(key: String, value: Dp) =
+    mutate { this[key] = value }
+
+private fun MutableState<Map<String, Dp?>>.mutate(mutator: MutableMap<String, Dp?>.()->Unit) {
+    this.value = value
+        .toMutableMap()
+        .also { it.mutator() }
+        .toMap()
+}
+
