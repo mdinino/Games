3260da0d4edd53126fb96495719ac43d8dafa326
Marc DiNino <dinino_marc@bah.com>
2025-06-22 18:17:41 -0400

    WIP

--- composeApp/src/commonMain/kotlin/dinino/marc/games/userflow/selectgame/ui/nav/SelectGameUserFlow.kt
+++ composeApp/src/commonMain/kotlin/dinino/marc/games/userflow/selectgame/ui/nav/SelectGameUserFlow.kt
@@ -1,8 +1,8 @@
 package dinino.marc.games.userflow.selectgame.ui.nav
 
 import dinino.marc.games.userflow.common.ui.SnackbarController
+import dinino.marc.games.userflow.common.ui.nav.SerializableUserFlowRoute
 import dinino.marc.games.userflow.common.ui.nav.SerializableUserFlowRoute.UserFlowNavGraphRoute
-import dinino.marc.games.userflow.common.ui.nav.SerializableUserFlowRoute.UserFlowScreenRoute
 import dinino.marc.games.userflow.common.ui.nav.UserFlow
 import dinino.marc.games.userflow.selectgame.di.selectGameSnackbarControllerQualifier
 import dinino.marc.games.userflow.selectgame.di.selectGameUserFLowModule
@@ -17,6 +17,6 @@ object SelectGameUserFlow: UserFlow, KoinComponent {
 
     override val navGraph = object : UserFlowNavGraphRoute {
         override val landingScreenRoute = SelectGameScreenRoute
-        override val otherRoutes: List<UserFlowScreenRoute> = emptyList()
+        override val otherRoutes: List<SerializableUserFlowRoute> = emptyList()
     }
 }
