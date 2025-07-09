96f18fa6a39efa6205d46f5c7ad6a09f545a8d8f
Marc DiNino <dinino_marc@bah.com>
2025-07-08 22:56:42 -0400

    Seems to be working

--- composeApp/src/commonMain/kotlin/dinino/marc/games/app/di/appModule.kt
+++ composeApp/src/commonMain/kotlin/dinino/marc/games/app/di/appModule.kt
@@ -1,10 +1,11 @@
 package dinino.marc.games.app.di
 
 import dinino.marc.games.Database
+import dinino.marc.games.platform.PlatformManager
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val appModule: Module =
     module {
-        single { Database(driver = get()) }
+        single { Database(get<PlatformManager>().databaseDriver) }
     }
