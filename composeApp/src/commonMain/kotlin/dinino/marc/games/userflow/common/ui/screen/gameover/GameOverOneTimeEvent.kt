bec50c223e72773f07d74889e7d28755f2eb8442
Marc DiNino <dinino_marc@bah.com>
2025-07-23 17:25:01 -0400

    Ficed

--- composeApp/src/commonMain/kotlin/dinino/marc/games/userflow/common/ui/screen/gameover/GameOverOneTimeEvent.kt
+++
@@ -1,9 +0,0 @@
-package dinino.marc.games.userflow.common.ui.screen.gameover
-
-import dinino.marc.games.userflow.common.ui.route.SerializableUserFlowRoute
-
-sealed interface GameOverOneTimeEvent {
-    interface Navigate: GameOverOneTimeEvent {
-        val route: SerializableUserFlowRoute
-    }
-}
--- composeApp/src/commonMain/kotlin/dinino/marc/games/userflow/common/ui/screen/selectneworresumegame/SelectNewOrResumeGameOneTimeEvent.kt
+++
@@ -1,9 +0,0 @@
-package dinino.marc.games.userflow.common.ui.screen.selectneworresumegame
-
-import dinino.marc.games.userflow.common.ui.route.UserFlowRouteEvent
-
-sealed interface SelectNewOrResumeGameOneTimeEvent {
-    interface Navigate: SelectNewOrResumeGameOneTimeEvent {
-        val routeEvent: UserFlowRouteEvent
-    }
-}
